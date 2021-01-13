b41aa8b7111ad59c5ba798cc8d6bdb99511d42c3
MarkWilber <mwilberssl@gmail.com>
2021-01-13 00:19:22 -0800

    Going round in circles with this. Back to original pickle.loads.

--- deployedApp/python/app.py
+++ deployedApp/python/app.py
@@ -39,11 +39,8 @@ def load_model(key):
     response = S3.get_object(Bucket=BUCKET_NAME, Key=key)
     model_str = response['Body'].read()
 
-    # model = pickle.loads(model_str)
-    with open('/tmp/' + model_str, 'wb') as outModelFile:
-        S3.Bucket(BUCKET_NAME, Key=key)\
-          .download_fileobj(model_str, outModelFile) with open('/tmp/' + model_str, 'rb') as inModelFile:
-            model = load(model_str)
+    model = pickle.loads(model_str)
+    # model = load(model_str)
 
     return model
 
