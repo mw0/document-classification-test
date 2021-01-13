88ab5ff277c8b323f333b0f3ef0034e77440548a
MarkWilber <mwilberssl@gmail.com>
2021-01-12 23:55:11 -0800

    Fiddled with download from S3 to tmp file, and then load tmp file. Looks dubious!

--- deployedApp/python/app.py
+++ deployedApp/python/app.py
@@ -40,7 +40,10 @@ def load_model(key):
     model_str = response['Body'].read()
 
     # model = pickle.loads(model_str)
-    model = load(model_str)
+    with open('/tmp/' + model_str, 'wb') as outModelFile:
+        S3.Bucket(BUCKET_NAME, Key=key)\
+          .download_fileobj(model_str, outModelFile) with open('/tmp/' + model_str, 'rb') as inModelFile:
+            model = load(model_str)
 
     return model
 
