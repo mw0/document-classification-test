cffc21eb48de751f3c5c14987db4d0705d9fe85f
MarkWilber <mwilberssl@gmail.com>
2021-01-14 23:41:06 -0800

    Actually added the build_and_push.sh and requirements.txt with this commit. Major changes to deploy2/model/predictor.py to make it much like that of deploy1/Classifier.

--- deploy2/model/predictor.py
+++ deploy2/model/predictor.py
@@ -1,35 +1,114 @@
 import os
-import pickle
+import json
 import io
 import flask
-import pandas as pd
+from joblib import load
+import logging
 
 model_path = os.environ['MODEL_PATH']
 
+# As XGBoost model only allows numerical results, so here's a dict to get
+# human-readable categories back:
+ind2category = {0: 'DELETION OF INTEREST',
+                1: 'RETURNED CHECK',
+                2: 'BILL',
+                3: 'POLICY CHANGE',
+                4: 'CANCELLATION NOTICE',
+                5: 'DECLARATION',
+                6: 'CHANGE ENDORSEMENT',
+                7: 'NON-RENEWAL NOTICE',
+                8: 'BINDER',
+                9: 'REINSTATEMENT NOTICE',
+                10: 'EXPIRATION NOTICE',
+                11: 'INTENT TO CANCEL NOTICE',
+                12: 'APPLICATION',
+                13: 'BILL BINDER'}
+
+# Logger format and location
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='log/testApp.log',
+                    filemode='w')
 
 # A singleton for holding the model. This simply loads the model and holds it.
-# It has a predict function that does a prediction based on the model and the input data.
+# It has a predict function that does a prediction based on the model and the
+# input data.
 
 class ScoringService(object):
-    model = None  # Where we keep the model when it's loaded
+    tfidf = None		# Where we keep the tfidfVectorizer
+    classifierNB = None		# Where we keep NaiveBayesClassifier
+    classifierGB = None		# Where we keep XGBoostClassifier
 
     @classmethod
-    def get_model(cls):
-        """Get the model object for this instance, loading it if it's not already loaded."""
-        if cls.model is None:
-            with open(os.path.join(model_path, 'model.pkl'), 'rb') as f:
-                cls.model = pickle.load(f)
-        return cls.model
+    def get_tfidf(cls):
+        """
+        Get the model object for this instance, loading it if it's not already
+        loaded.
+        """
+        print("You hit get_tfidf()!")
+        if cls.tfidf is None:
+            with open(os.path.join(model_path,
+                                   'tfidfVectorizer.pkl'), 'rb') as f:
+                cls.tfidf = load(f)
+        print(f"type(cls.tfidf): {type(cls.tfidf)}")
+        return cls.tfidf
 
     @classmethod
-    def predict(cls, x):
+    def get_classifierNB(cls):
+        """
+        Get the model object for this instance, loading it if it's not already
+        loaded.
+        """
+        print("You hit get_classifierNB()!")
+        if cls.classifierNB is None:
+            with open(os.path.join(model_path,
+                                   'ComplementNaiveBayes0.pkl'), 'rb') as f:
+                cls.classifierNB = load(f)
+        print(f"type(cls.classifierNB): {type(cls.classifierNB)}")
+        return cls.classifierNB
+
+    @classmethod
+    def get_classifierGB(cls):
+        """
+        Get the model object for this instance, loading it if it's not already
+        loaded.
+        """
+        print("You hit get_classifierGB()!")
+        if cls.classifierGB is None:
+            with open(os.path.join(model_path,
+                                   'GradientBoostBest.pkl'), 'rb') as f:
+                cls.classifierGB = load(f)
+        print(f"type(cls.classifierGB): {type(cls.classifierGB)}")
+        return cls.classifierGB
+
+    @classmethod
+    def predict(cls, modelName, stringList):
         """For the input, do the predictions and return them.
 
         Args:
-            input (a pandas dataframe): The data on which to do the predictions. There will be
-                one prediction per row in the dataframe"""
-        clf = cls.get_model()
-        return clf.predict(x)
+        input (a pandas dataframe): The data on which to do the
+        predictions. There will be one prediction per row in the dataframe
+        """
+
+        print("You hit get_predict()!")
+        tfidf = cls.get_tfidf()
+        classifierNB = cls.get_classifierNB()
+        classifierGB = cls.get_classifierGB()
+
+        X = tfidf.transform(stringList)
+
+        if modelName == 'NaiveBayes':
+            predictions = classifierNB.predict(X)
+        elif modelName == 'XGBoosted':
+            predictions = [ind2category(p) for p in classifierGB.predict(X)]
+        else:
+            return flask.Response(response='Bad Request', status=400,
+                                  mimetype='text/plain')
+
+        print("categories:\n", predictions)
+
+        return predictions
 
 
 # The flask app for serving predictions
@@ -38,38 +117,53 @@ app = flask.Flask(__name__)
 
 @app.route('/ping', methods=['GET'])
 def ping():
-    """Determine if the container is working and healthy. In this sample container, we declare
-    it healthy if we can load the model successfully."""
-    health = ScoringService.get_model() is not None  # You can insert a health check here
+    """
+    Determine if the container is working and healthy. In this sample
+    container, we declare it healthy if we can load the model successfully.
+    """
+
+    # You can insert a health check here
+    print("You hit ping!")
+    health = ((ScoringService.get_tfidf() is not None)
+              and (ScoringService.get_classifierNB() is not None)
+              and (ScoringService.get_classifierGB() is not None))
+    print(f"health: {health}")
 
     status = 200 if health else 404
-    return flask.Response(response='\n', status=status, mimetype='application/json')
+    print(f"status: {status}")
+    return flask.Response(response='\n', status=status,
+                          mimetype='application/json')
 
 
 @app.route('/invocations', methods=['POST'])
-def transformation():
-    """Do an inference on a single batch of data. In this sample server, we take data as CSV, convert
-    it to a pandas data frame for internal use and then convert the predictions back to CSV (which really
-    just means one prediction per line, since there's a single column.
+def invocations():
     """
-    data = None
+    Get JSON input and extract modelName and stringList
+    """
+    input_json = flask.request.get_json()
 
-    # Convert from CSV to pandas
-    if flask.request.content_type == 'text/csv':
-        data = flask.request.data.decode('utf-8')
-        s = io.StringIO(data)
-        data = pd.read_csv(s, header=None)
-    else:
-        return flask.Response(response='This predictor only supports CSV data', status=415, mimetype='text/plain')
+    modelName = input_json['model']
+    logging.info(f"modelName: {modelName}.")
 
-    print('Invoked with {} records'.format(data.shape[0]))
+    stringList = input_json['strings']
+    print(stringList[0])
+
+    print(f'Invoked with {len(stringList)} records')
 
     # Do the prediction
-    predictions = ScoringService.predict(data)
+    predictions = ScoringService.predict(modelName, stringList)
 
-    # Convert from numpy back to CSV
-    out = io.StringIO()
-    pd.DataFrame({'results': predictions}).to_csv(out, header=False, index=False)
-    result = out.getvalue()
+    # Transform predictions to JSON
+    result = {
+        'model': f"{modelName}",
+        'output': list(predictions)
+        }
 
-    return flask.Response(response=result, status=200, mimetype='text/csv')
+    resultjson = json.dumps(result)
+    return flask.Response(response=resultjson, status=200,
+                          mimetype='application/json')
+
+
+if __name__ == '__main__':    
+    # listen on all IPs 
+    app.run(host='0.0.0.0', debug=False, port=5000)
