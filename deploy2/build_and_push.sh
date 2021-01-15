8098a695285e25e92de51ec747b75c73d0e7c5d6
MarkWilber <mwilberssl@gmail.com>
2021-01-14 23:57:24 -0800

    Small fixes, and many echos added.

--- deploy2/build_and_push.sh
+++ deploy2/build_and_push.sh
@@ -15,6 +15,7 @@ chmod +x model/serve
 
 # Get the account number associated with the current IAM credentials
 account=$(aws sts get-caller-identity --query Account --output text)
+echo "account: $account"
 
 if [ $? -ne 0 ]
 then
@@ -26,8 +27,11 @@ fi
 # us-west-1 == ireland
 region=$(aws configure get region)
 region=${region:-us-east-1}
+echo "region: $region"
 
-fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:latest"
+#fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:latest"
+fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}"
+echo "fullname: $fullname"
 
 # If the repository doesn't exist in ECR, create it.
 
@@ -35,16 +39,20 @@ aws ecr describe-repositories --repository-names "${image}" > /dev/null 2>&1
 
 if [ $? -ne 0 ]
 then
+    echo "creating repository  ..."
     aws ecr create-repository --repository-name "${image}" > /dev/null
 fi
 
 # Get the login command from ECR and execute it directly
-$(aws ecr get-login --region ${region} --no-include-email)
+$(aws ecr get-login-password  --region ${region} --no-include-email)
 
 # Build the docker image locally with the image name and then push it to ECR
 # with the full name.
 
+echo "building image ..."
 docker build  -t ${image} .
 docker tag ${image} ${fullname}
 
+echo "pushing image ..."
 docker push ${fullname}
+echo "Done!"
